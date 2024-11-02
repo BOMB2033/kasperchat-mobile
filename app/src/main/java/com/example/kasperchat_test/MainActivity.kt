7e712204c732d49b96bb83ad0f707edab8a91ffd
Fedor Kasper <onymimik@rambler.ru>
2024-11-02 11:07:45 +0300

    Накидал основу для дальнейших тестов сервера

--- app/src/main/java/com/example/kasperchat_test/MainActivity.kt
+++ app/src/main/java/com/example/kasperchat_test/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.kasperchat_test
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kasperchat_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
+    private val items = mutableListOf<String>()
+    private lateinit var adapter: ArrayAdapter<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, items)
+            listViewMessages.adapter = adapter
+            buttonSend.setOnClickListener {
+                val text = editTextMessage.text.toString()
+                if (text.isNotEmpty()) {
+                    items.add(text)
+                    adapter.notifyDataSetChanged()
+                    editTextMessage.text.clear()
+                }
+            }
+
         }
     }
 }
