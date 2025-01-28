751fa2abd4d23fa0442ca2891bf98d65e796d52f
The <124300265+yozhikzver@users.noreply.github.com>
2025-01-28 21:50:01 +0300

    Design start <!-- Начало дизайна формы входа: добавление кнопок --> Ничё не робит

--- app/src/main/java/com/example/kasperchat_test/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/kasperchat_test/fragment/LoginFragment.kt
@@ -40,9 +40,9 @@ class LoginFragment : Fragment() {
             buttonLogin.setOnClickListener {
                 it.findNavController().navigate(R.id.action_loginFragment_to_chatsListFragment)
             }
-            textViewRegistration.setOnClickListener {
+            /*textViewRegistration.setOnClickListener {
                 it.findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
-            }
+            }*/
         }
     }
 
