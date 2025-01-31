cb587af8ef6c063223705303879adfc636c7f900
Fedor Kasper <65659832+BOMB2033@users.noreply.github.com>
2025-01-31 06:42:58 +0300

    Реализация сетевого интерфейса

--- app/src/main/java/com/example/kasperchat_test/activity/MainActivity.kt
+++ app/src/main/java/com/example/kasperchat_test/activity/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.kasperchat_test.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kasperchat_test.databinding.ActivityMainBinding
+import com.example.kasperchat_test.network.SocketManager
+import com.example.kasperchat_test.network.SocketManagerInterface
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),SocketManagerInterface {
     private lateinit var binding: ActivityMainBinding
+    override val socketManager:SocketManager = SocketManager(applicationContext)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
