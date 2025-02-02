534066c9349a4f085bc3594bf396eecf742c094b
fedorkasper <kasper_f@list.ru>
2025-02-03 01:56:08 +0300

    Создание бд

--- app/src/main/java/com/example/kasperchat_test/activity/MainActivity.kt
+++ app/src/main/java/com/example/kasperchat_test/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kasperchat_test.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kasperchat_test.database.MyApplication
+import com.example.kasperchat_test.database.User
 import com.example.kasperchat_test.databinding.ActivityMainBinding
 import com.example.kasperchat_test.network.SocketManager
 import com.example.kasperchat_test.network.SocketManagerInterface
@@ -14,5 +16,27 @@ class MainActivity : AppCompatActivity(),SocketManagerInterface {
         socketManager = SocketManager(applicationContext)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        socketManager  = SocketManager(applicationContext)
+
+        // Получение экземпляра DAO
+        val userDao = (application as MyApplication).database.userDao()
+
+// Вставка данных
+        val user = User(firstName= "John", lastName = "Doe")
+        userDao.insertAll(user)
+
+// Получение всех пользователей
+        val users = userDao.getAll()
+
+// Поиск пользователя по имени
+        val foundUser = userDao.findByName("John", "Doe")
+
+// Обновление пользователя
+        val updatedUser = User(id = foundUser.id, firstName = "Updated John", lastName = "Updated Doe")
+        userDao.update(updatedUser)
+
+// Удаление пользователя
+        userDao.delete(updatedUser)
+
     }
 }
